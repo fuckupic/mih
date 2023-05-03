3608d16e4b4098a6911eae3fda241acb8bf1a200
Tadeáš Kapic <mujmeyl@gmail.com>
2023-05-03 14:38:38 +0200

    eslint ignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ignoreBuildErrors: true,
+  ignoreDuringBuilds: true,
 }
 
 module.exports = nextConfig
--- src/types/canvas-sketch.d.ts
+++ src/types/canvas-sketch.d.ts
@@ -1,3 +1,6 @@
 declare module 'canvas-sketch'
 declare module 'canvas-sketch-util/random'
 declare module 'canvas-sketch-util/math'
+
+declare module 'gsap/dist/gsap'
+declare module 'gsap/dist/ScrollTrigger'
