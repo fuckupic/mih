e774708fc148d210e4158076e2e0ab70bd9db319
Tadeáš Kapic <mujmeyl@gmail.com>
2023-05-15 12:58:36 +0200

    Delete SmootherContext

--- src/pages/SmootherContext.tsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from 'react'
-import ScrollSmoother from 'ScrollSmoother'
-
-// A default smoother instance or null can be passed.
-export const SmootherContext = createContext<ScrollSmoother | null>(null)
