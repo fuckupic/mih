7fa1f659353f1ff139fd73a761e93589ec10577e
Tadeáš Kapic <mujmeyl@gmail.com>
2023-06-20 09:09:22 +0200

    remove resize request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,17 @@ import Hero from '../components/Hero/Hero'
 import CityBackground from '@/components/ScrollingSections/CityBackground'
 import BackgroundOrb from '@/components/ScrollingSections/BackgroundOrb'
 import ForegroundOrb from '@/components/ScrollingSections/ForegroundOrb'
+
 import Cursor from '@/components/Cursor/Cursor'
+
 import ScrollingSections from '../components/ScrollingSections/ScrollingSections'
 
 const IndexPage: React.FC = () => {
   const [isMobile, setIsMobile] = useState(false)
-  const [renderKey, setRenderKey] = useState(Date.now())
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768)
-      setRenderKey(Date.now()) // Set new render key to force re-render
     }
 
     handleResize() // Check initial screen size
@@ -23,16 +23,15 @@ const IndexPage: React.FC = () => {
       window.removeEventListener('resize', handleResize)
     }
   }, [])
-
   return (
-    <div key={renderKey}>
+    <>
       {!isMobile && <Cursor />}
       <Hero />
       <BackgroundOrb />
       <ForegroundOrb />
       <ScrollingSections />
       <CityBackground />
-    </div>
+    </>
   )
 }
 
